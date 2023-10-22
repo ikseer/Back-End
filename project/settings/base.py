11d46cceb26e9e30dced87b0eaf0c0ea6fab85e9
MohamedHamed12 <mh1779371@gmail.com>
2023-10-22 21:27:11 +0300

    vercel

--- project/settings/base.py
+++ project/settings/base.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -205,7 +205,7 @@ INSTALLED_APPS+= [
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR,  'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 ###### database #######
@@ -223,8 +223,8 @@ DATABASES = {
 }
 
 ###### Media #######
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
