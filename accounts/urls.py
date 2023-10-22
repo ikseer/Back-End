e238eaf3dfc3cf36c16d6a6247acfd4d8094ea23
MohamedHamed12 <mh1779371@gmail.com>
2023-10-22 18:56:20 +0300

    database_supabase

--- accounts/urls.py
+++ accounts/urls.py
@@ -19,5 +19,8 @@ urlpatterns = [
     path('rest-auth/password/reset/confirm/<str:uidb64>/<str:token>', PasswordResetConfirmView.as_view(),
             name='password_reset_confirm'),
 ]
+from django.conf import settings
+from django.conf.urls.static import static
 
-
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
