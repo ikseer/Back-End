5bafa26674f8768d31aac182ad68062cfcb8d0a4
MohamedHamed12 <mh1779371@gmail.com>
2023-10-22 19:04:11 +0300

    database_supabase

--- project/urls.py
+++ project/urls.py
@@ -19,7 +19,8 @@ from django.urls import include, path
 from .utils import schema_view
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls'))
@@ -34,8 +35,7 @@ urlpatterns += [
 ]
 
 
-from django.conf import settings
-from django.conf.urls.static import static
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
