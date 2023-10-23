01b8af70a5666c902a977cd16f5bbc446cd56b46
MohamedHamed12 <mh1779371@gmail.com>
2023-10-23 22:19:24 +0300

    otp

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,22 +4,21 @@ from django.urls import include, path
 from accounts.views import CustomRegisterView, FacebookLogin, GoogleLogin, getPhoneNumberRegistered_TimeBased
 from dj_rest_auth.views import PasswordResetView, PasswordResetConfirmView
 urlpatterns = [
-        path("phone/<phone>/", getPhoneNumberRegistered_TimeBased.as_view(), name="OTP_Gen"),
+    ### phone ###
+    path("phone/<phone>/", getPhoneNumberRegistered_TimeBased.as_view(), name="OTP_Gen"),
 
-    path('', include('dj_rest_auth.urls')),
-    # path('password/reset/', PasswordResetView.as_view(), name='rest_password_reset'),
-    # path('password/reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-
-    path('register/', CustomRegisterView.as_view(), name='rest_register'),
-    # path('register/', include('dj_rest_auth.registration.urls')),
+    ### social  ###
     path('dj-rest-auth/facebook/', FacebookLogin.as_view(), name='fb_login'),
     path('dj-rest-auth/google/', GoogleLogin.as_view(), name='google_login'),
+
+    ### dj-rest-auth ###
+    path('', include('dj_rest_auth.urls')),
+    path('register/', CustomRegisterView.as_view(), name='rest_register'),
+    path('rest-auth/password/reset/confirm/<str:uidb64>/<str:token>', PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    
+    ### allauth ###
     path('allauth/', include('allauth.urls')),
-    # path('rest-auth/password/reset/confirm/<str:uidb64>/<str:token>', PasswordResetConfirmView.as_view(),
-    #         name='password_reset_confirm'),
-    # path('password/reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('rest-auth/password/reset/confirm/<str:uidb64>/<str:token>', PasswordResetConfirmView.as_view(),
-            name='password_reset_confirm'),
+
 ]
 from django.conf import settings
 from django.conf.urls.static import static
