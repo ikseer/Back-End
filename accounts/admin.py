8585280b7e60006a059570292fc4a82a82a14d74
MohamedHamed12 <mh1779371@gmail.com>
2024-02-04 17:12:59 +0200

    admin page profile

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,4 +4,7 @@ from accounts.models import PhoneModel, Profile
 
 # Register your models here.
 admin.site.register(PhoneModel)
-admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display=('user','image','bio','first_name','last_name','date_of_birth','gender','is_completed','timezone')
+
+admin.site.register(Profile,ProfileAdmin)
