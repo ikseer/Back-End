ba4a77e0e2f9204972e594437aa9ef1bde79887b
MohamedHamed12 <mh1779371@gmail.com>
2024-02-09 18:22:58 +0200

    edit swagger urls

--- project/utils.py
+++ project/utils.py
@@ -3,7 +3,7 @@ from django.urls import re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from  rest_framework.schemas.generators import OpenAPISchemaGenerator
+from drf_yasg.generators import OpenAPISchemaGenerator
 class BothHttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator):
     def get_schema(self, request=None, public=False):
         schema = super().get_schema(request, public)
