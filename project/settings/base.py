b5c0fc8a6943bfe5fbf97d3e2391dbd8e94e9a67
MohamedHamed12 <mh1779371@gmail.com>
2024-02-10 18:37:40 +0200

    stop csrf for development

--- project/settings/base.py
+++ project/settings/base.py
@@ -76,7 +76,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -345,3 +345,4 @@ else:
         }
 
     }   
+
