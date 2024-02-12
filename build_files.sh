88bfe02b7cb78862b61598f0f1d09365d61c3e72
MohamedHamed12 <mh1779371@gmail.com>
2024-02-12 13:50:32 +0200

    add docker-compose

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
-# build_files.sh
-pip install -r requirements.txt
-python3 manage.py collectstatic
+#!/bin/bash
+python3 manage.py makemigrations
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput
+gunicorn --config gunicorn_config.py project.wsgi:application
