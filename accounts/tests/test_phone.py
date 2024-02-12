c63fc2aa759ed6f66837c9efc500015b808079db
MohamedHamed12 <mh1779371@gmail.com>
2024-02-12 13:46:22 +0200

    edit test for phone number

--- accounts/tests/test_phone.py
+++ accounts/tests/test_phone.py
@@ -20,24 +20,31 @@ class GetPhoneNumberRegisteredTimeBasedTestCase(TestCase):
         self.client = APIClient()
         self.phone_number = "1234567890"  # Replace with a phone number you want to use
         user = User.objects.create_user(username='testuser',email='test@example.com', password='testpassword')
+        # get access token
+        response = self.client.post(reverse('rest_login'), {'email': 'test@example.com', 'password': 'testpassword'})
+        self.access = response.data['access']
+        # self.client.force_authenticate(user=user)
 
-        self.client.force_authenticate(user=user)
 
-    def test_get_request_creates_PhoneModel_if_not_exists(self):
-      
-        # response = self.client.get(reverse('OTP_Gen', args=[self.phone_number]))
+    def test_register_phone_number_not_auth(self):
         response = self.client.post(reverse('phone-register'), {"phone": self.phone_number})
-       
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_register_phone_number(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access}')
+        response = self.client.post(reverse('phone-register'), {"phone": self.phone_number})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertTrue(PhoneModel.objects.filter(Mobile=self.phone_number).exists())
 
     def test_get_request_returns_otp(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access}')
         response = self.client.post(reverse('phone-register'), {"phone": self.phone_number})
         # print(response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn("OTP" , response.data)
 
     def test_post_request_with_valid_otp(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access}')
         response = self.client.post(reverse('phone-register'), {"phone": self.phone_number})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         otp=response.data["OTP"]
@@ -45,28 +52,23 @@ class GetPhoneNumberRegisteredTimeBasedTestCase(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         mobile = PhoneModel.objects.get(Mobile=self.phone_number)
         self.assertTrue(mobile.isVerified)
-    # def test_post_request_with_valid_otp(self):
-    #     self.client.get(reverse('OTP_Gen', args=[self.phone_number]))
-    #     idx=mail.outbox[0].body.find('Your OTP is:')
-    #     otp=mail.outbox[0].body[idx:]
-    #     response = self.client.post(reverse('OTP_Gen', args=[self.phone_number]), {"otp": otp})
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
     def test_post_request_with_invalid_otp(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access}')
         PhoneModel.objects.create(Mobile=self.phone_number)
         otp = "000000"  # Replace with an invalid OTP
         response = self.client.post(reverse('verify-mobile-otp'), {"otp": otp, "phone": self.phone_number})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_post_request_with_nonexistent_phone_number(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access}')
         response = self.client.post(reverse('verify-mobile-otp'), {"otp": "123456", "phone": "9999999999"})
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_post_request_with_verified_user(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access}')
         PhoneModel.objects.create(Mobile=self.phone_number, isVerified=True)
         otp = "123456"  
         response = self.client.post(reverse('verify-mobile-otp'), {"otp": otp, "phone": self.phone_number})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-# # # Ensure to update the URL name ('OTP_Gen') in the reverse calls.
