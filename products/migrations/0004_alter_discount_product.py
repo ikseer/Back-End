40fa72356d9184ce59b847658139b7bb548acfa6
MohamedHamed12 <mh1779371@gmail.com>
2024-02-16 10:36:23 +0200

    add pre-commit

---
+++ products/migrations/0004_alter_discount_product.py
@@ -0,0 +1,20 @@
+import django.db.models.deletion
+from django.db import migrations
+from django.db import models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("products", "0003_remove_product_orders_delete_orderitem"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="discount",
+            name="product",
+            field=models.OneToOneField(
+                on_delete=django.db.models.deletion.CASCADE, to="products.product"
+            ),
+        ),
+    ]
