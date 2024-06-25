db8ecedf808c136c6868de07f6bbfb7605a21292
MohamedHamed12 <mh1779371@gmail.com>
2024-06-25 09:01:42 +0300

    fix database fields add new fields

--- project/orders/models/order.py
+++ project/orders/models/order.py
@@ -13,8 +13,11 @@ class Order(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders',null=True,blank=True)
     items = models.ManyToManyField("products.Product", through='OrderItem')
     total_price = models.DecimalField(max_digits=10, decimal_places=2,null=True,blank=True)
-    # created_at = models.DateTimeField(auto_now_add=True)
-    # updated_at = models.DateTimeField(auto_now=True)
+
+    owner = models.CharField(max_length=255,  null=True, blank=True)
+    location= models.CharField(max_length=255, null=True, blank=True)
+    phone=models.CharField(max_length=255, null=True, blank=True)
+
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
     pharmacy = models.ForeignKey(
         "pharmacy.Pharmacy", on_delete=models.SET_NULL, null=True, blank=True
