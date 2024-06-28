6b3d5d3a9d624e82891213fd2129ba4712955b80
MohamedHamed12 <mh1779371@gmail.com>
2024-06-28 21:19:51 +0300

    add pagination

--- project/accounts/pagination.py
+++ project/accounts/pagination.py
@@ -2,6 +2,6 @@
 from rest_framework.pagination import PageNumberPagination
 
 
-class ProductPagination(PageNumberPagination):
+class CustomPagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = "limit"
