7c1b4fb72cf1be5ebb3d6db1957fb44ec2fbe49b
MohamedHamed12 <mh1779371@gmail.com>
2024-07-01 08:24:20 +0300

    fix docs patient post

--- project/accounts/permissions.py
+++ project/accounts/permissions.py
@@ -5,8 +5,8 @@ from rest_framework.permissions import BasePermission
 
 class ProfilePermission(BasePermission):
     def has_permission(self, request, view):
-        if not request.user.is_authenticated :
-                return False
+
+        return request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         if request.user.is_staff:
