b81d07710ae55d07e2e9acf1de578bffe9984089
MohamedHamed12 <mh1779371@gmail.com>
2024-07-02 05:40:32 +0300

    edit paymob save

--- project/orders/serializers/paymob.py
+++ project/orders/serializers/paymob.py
@@ -6,10 +6,16 @@ from rest_framework import serializers
 
 
 class PaymobOrderSerializer(serializers.ModelSerializer):
-    paymob_order_id=serializers.CharField(read_only=True)
-    paid=serializers.CharField(read_only=True)
-    amount_cents=serializers.CharField(read_only=True)
-    currency=serializers.CharField(read_only=True)
+
+    class Meta:
+        model = PaymobOrder
+        fields = "__all__"
+        read_only_fields=['paymob_order_id','paid','amount_cents','currency']
+
+
+
+class SavePaymobOrderSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = PaymobOrder
         fields = "__all__"
