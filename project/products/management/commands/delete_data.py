36be8b75e6d1e7aadd4b7b689122af89e0266b18
MohamedHamed12 <mh1779371@gmail.com>
2024-07-03 20:50:51 +0300

    fix cart

--- project/products/management/commands/delete_data.py
+++ project/products/management/commands/delete_data.py
@@ -4,6 +4,9 @@ import json
 from django.apps import apps
 from django.core.management.base import BaseCommand
 
+from accounts.models.patient import Patient
+
+from safedelete import HARD_DELETE
 
 class Command(BaseCommand):
     help = 'Export data from all models in the database'
@@ -12,7 +15,10 @@ class Command(BaseCommand):
         from accounts.models import CustomUser
 
         # Get users where 'delete' is not null
-        users_to_delete = CustomUser.objects.exclude(delete__isnull=True)
-
-        # Delete the users
-        users_to_delete.delete()
+        # users_to_delete = .deleted_objects.all()
+        users_to_delete = CustomUser.deleted_objects.all()
+        print(len(users_to_delete))
+        for user in users_to_delete:
+            user.delete(force_policy=HARD_DELETE)
+        # Delete the userusers_to_deletes
+        # users_to_delete.delete()
