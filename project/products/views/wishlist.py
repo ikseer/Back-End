76d61cbff9c0171e4fef546b999bf49ad5ab4905
MohamedHamed12 <mh1779371@gmail.com>
2024-07-04 19:27:52 +0300

    edit permissions

--- project/products/views/wishlist.py
+++ project/products/views/wishlist.py
@@ -9,6 +9,7 @@ from products.pagination import *
 from products.permissions import *
 from products.serializers import *
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 use_cache=config('use_cache',0)
 
@@ -16,6 +17,6 @@ use_cache=config('use_cache',0)
 class WishlistViewSet(viewsets.ModelViewSet):
     queryset = Wishlist.objects.all()
     serializer_class = WishlistSerializer
-    permission_classes = [SafePermission]
+    permission_classes = [IsAuthenticated]
     filterset_class = WishlistFilter
     pagination_class = ProductPagination
